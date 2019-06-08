4ea3257a359f45abee322381a776ef137fb4e6e6
StigUK <prusbogdan2@gmail.com>
2019-06-08 20:05:24 +0300

    don't work

--- app/src/main/java/prusbogdan/carent/ViewOrderActivity.java
+++ app/src/main/java/prusbogdan/carent/ViewOrderActivity.java
@@ -1,12 +1,16 @@
 package prusbogdan.carent;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,14 +20,30 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wallet.AutoResolveHelper;
+import com.google.android.gms.wallet.IsReadyToPayRequest;
+import com.google.android.gms.wallet.PaymentData;
+import com.google.android.gms.wallet.PaymentDataRequest;
+import com.google.android.gms.wallet.PaymentsClient;
+import com.google.android.gms.wallet.Wallet;
+import com.google.android.gms.wallet.WalletConstants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectOutputStream;
+import java.util.Optional;
 
 import prusbogdan.carent.Classes.Banlist;
+import prusbogdan.carent.Classes.GooglePay;
 import prusbogdan.carent.Classes.Order;
 import prusbogdan.carent.Classes.User;
 import retrofit2.Call;
@@ -34,10 +54,15 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ViewOrderActivity extends AppCompatActivity {
 
+
+    private PaymentsClient mPaymentsClient;
+    private Button mGooglePayButton;
+    private static final int LOAD_PAYMENT_DATA_REQUEST_CODE = 42;
     Data data;
     Retrofit retrofit;
     Api api;
     Context context;
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +73,6 @@ public class ViewOrderActivity extends AppCompatActivity {
         Button cancel = findViewById(R.id.vieworder_cancel);
         Button pay = findViewById(R.id.vieworder_pay);
         context = this;
-
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -116,13 +140,144 @@ public class ViewOrderActivity extends AppCompatActivity {
                 alert.show();
             }
         });
+        /*IsReadyToPayRequest readyToPayRequest = null;
+        try {
+            readyToPayRequest = IsReadyToPayRequest.fromJson(baseConfigurationJson().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Task<Boolean> task = paymentsClient.isReadyToPay(readyToPayRequest);
+        task.addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
+            @Override
+            public void onComplete(@NonNull Task<Boolean> completeTask) {
+                if(completeTask.isSuccessful())
+                {
+                    //showGooglePayButton(completeTask.getResult());
+                }
+                else
+                {
+                    //
+                }
+            }
+        });*/
+
+        mPaymentsClient =
+                Wallet.getPaymentsClient(
+                        this,
+                        new Wallet.WalletOptions.Builder()
+                                .setEnvironment(WalletConstants.ENVIRONMENT_TEST)
+                                .build());
+
+        //possiblyShowGooglePayButton();
+
+
+
         pay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 check_user();
+                requestPayment(v);
             }
         });
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void possiblyShowGooglePayButton() {
+        final Optional<JSONObject> isReadyToPayJson = GooglePay.getIsReadyToPayRequest();
+        if (!isReadyToPayJson.isPresent()) {
+            return;
+        }
+        IsReadyToPayRequest request = IsReadyToPayRequest.fromJson(isReadyToPayJson.get().toString());
+        if (request == null) {
+
+            return;
+        }
+
+        Task<Boolean> task = mPaymentsClient.isReadyToPay(request);
+        task.addOnCompleteListener(
+                new OnCompleteListener<Boolean>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Boolean> task) {
+                        try {
+                            error1("1");
+                            boolean result = task.getResult(ApiException.class);
+                            if (result) {
+                                // show Google as a payment option
+                                mGooglePayButton = findViewById(R.id.vieworder_pay);
+                                mGooglePayButton.setOnClickListener(
+                                        new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view) {
+                                                requestPayment(view);
+                                                error1("a");
+                                            }
+                                        });
+                                mGooglePayButton.setVisibility(View.VISIBLE);
+                            }
+                        } catch (ApiException exception) {
+                            error(exception.toString());
+                            // handle developer errors
+                        }
+                    }
+                });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void requestPayment(View view) {
+        Optional<JSONObject> paymentDataRequestJson = GooglePay.getPaymentDataRequest();
+        if (!paymentDataRequestJson.isPresent()) {
+            return;
+        }
+        PaymentDataRequest request =
+                PaymentDataRequest.fromJson(paymentDataRequestJson.get().toString());
+        if (request != null) {
+            AutoResolveHelper.resolveTask(
+                    mPaymentsClient.loadPaymentData(request), this, LOAD_PAYMENT_DATA_REQUEST_CODE);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            // value passed in AutoResolveHelper
+            case LOAD_PAYMENT_DATA_REQUEST_CODE:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        PaymentData paymentData = PaymentData.getFromIntent(data);
+                        String json = paymentData.toJson();
+                        // if using gateway tokenization, pass this token without modification
+                        JSONObject paymentMethodData = null;
+                        try {
+                            paymentMethodData = new JSONObject(json)
+                                    .getJSONObject("paymentMethodData");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            String paymentToken = ((JSONObject) paymentMethodData)
+                                    .getJSONObject("tokenizationData")
+                                    .getString("token");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        break;
+                    case AutoResolveHelper.RESULT_ERROR:
+                        Status status = AutoResolveHelper.getStatusFromIntent(data);
+                        // Log the status for debugging.
+                        // Generally, there is no need to show an error to the user.
+                        // The Google Pay payment sheet will present any account errors.
+                        break;
+                    default:
+                        // Do nothing.
+                }
+                break;
+            default:
+                // Do nothing.
+        }
+    }
+
 
     @SuppressLint("SetTextI18n")
     private void setFields()
@@ -350,5 +505,36 @@ public class ViewOrderActivity extends AppCompatActivity {
         alert.setView(image);
         alert.setMessage(text);
         alert.show();
+    }
+
+    private static JSONObject baseConfigurationJson() throws JSONException {
+        return new JSONObject()
+                .put("apiVersion",2)
+                .put("apiVersionMinor", 0)
+                .put("allowedPaymentsMethods", new JSONArray().put(getCardPaymentMethod()));
+    }
+
+    private void showGooglePayButton(boolean userIsReadyToPay){
+        if(userIsReadyToPay)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    private static JSONObject getCardPaymentMethod() throws JSONException {
+        final String[] networks = new String[] {"VISA", "AMEX"};
+        final String[] authMethods =
+                new String[] {"PAN_ONLY", "CRYPTOGRAM_3DS"};
+        JSONObject card = new JSONObject();
+        card.put("type", "CARD");
+        //card.put("tokenizationSpecification", getTokenizationSpec());
+        card.put("parameters", new JSONObject()
+        .put("allowedAuthMethods", new JSONArray(authMethods))
+                .put("allowedCardNetworks", new JSONArray(networks)));
+        return card;
     }
 }
